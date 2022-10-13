8ace8ba8100ffea1eb418c0faa61008867bef8b3
gabe <playwigs@gmail.com>
2022-10-12 21:34:10 -0600

    Created Pet model.

--- models.py
+++ models.py
@@ -10,3 +10,16 @@ def connect_db(app):
 
     with app.app_context():
         db.create_all()
+
+class Pet(db.Model):
+    """Pet info."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False, default=True)
