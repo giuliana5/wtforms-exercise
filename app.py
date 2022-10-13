596dd02a4a6178b6737d6622f147cf655da30c57
gabe <playwigs@gmail.com>
2022-10-12 21:31:22 -0600

    Created route for homepage to list pets in db.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db
+from models import connect_db, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
@@ -10,3 +10,11 @@ app.config['SECRET_KEY'] = 'secret'
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route("/")
+def homepage():
+    """Home page displays a list of pets."""
+
+    pets = Pet.query.all()
+
+    return render_template("homepage.html", pets=pets)
