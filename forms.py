6e8a9fc5d8e1cf16743e8cfeb1d40ff87d3d79cf
gabe <playwigs@gmail.com>
2022-10-14 15:19:43 -0600

    Updated validators & changed StringField to SelectField for species.

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, URLField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, IntegerField, URLField, SelectField
+from wtforms.validators import InputRequired, Optional, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form to add a new pet."""
 
     name = StringField("Pet Name", validators=[InputRequired()])
-    species = StringField("Species", validators=[InputRequired()])
+    species = SelectField("Species", choices=["Cat", "Dog", "Porcupine"])
     photo_url = URLField("Image URL")
-    age = IntegerField("Age", validators=[Optional()])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30, message="Invalid Age")])
     notes = StringField("Notes")
