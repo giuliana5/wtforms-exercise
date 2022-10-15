731b8a55b876485c4b55342be9dd325d63293cb0
gabe <playwigs@gmail.com>
2022-10-14 19:50:13 -0600

    Created route to display pet page and handle form to edit pet info.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, Pet, db
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
 
-debug = DebugToolbarExtension(app)
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+default_img = "https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif"
 
 @app.route("/")
 def homepage():
@@ -25,10 +26,11 @@ def add_pet():
     """Creates form to add a pet..."""
 
     form = AddPetForm()
+    
     if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
-        photo_url = form.photo_url.data or "https://thecontemporarypet.com/wp-content/themes/contemporarypet/images/default.png"
+        photo_url = form.photo_url.data or default_img
         age = form.age.data
         notes = form.notes.data
 
@@ -40,3 +42,22 @@ def add_pet():
         return redirect("/")
     else:
         return render_template("add-pet.html", form=form)
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def pet_page(pet_id):
+    """Displays info about the pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+
+        db.session.add(pet)
+        db.session.commit()
+
+        return redirect("/")
+    else:
+        return render_template("pet-page.html", pet=pet, form=form)
