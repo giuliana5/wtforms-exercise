ed0ed96f98e07baeafc8d2dec2da6170472fac64
gabe <playwigs@gmail.com>
2022-10-14 19:47:15 -0600

    Added EditPetForm to display fewer inputs.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, URLField, SelectField
+from wtforms import StringField, IntegerField, URLField, SelectField, BooleanField
 from wtforms.validators import InputRequired, Optional, NumberRange
 
 class AddPetForm(FlaskForm):
@@ -7,6 +7,14 @@ class AddPetForm(FlaskForm):
 
     name = StringField("Pet Name", validators=[InputRequired()])
     species = SelectField("Species", choices=["Cat", "Dog", "Porcupine"])
-    photo_url = URLField("Image URL")
+    photo_url = URLField("Image URL", validators=[Optional()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30, message="Invalid Age")])
-    notes = StringField("Notes")
+    notes = StringField("Notes", validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """Form to edit existing pet."""
+
+    photo_url = URLField("Image URL", validators=[Optional()])
+    notes = StringField("Notes", validators=[Optional()])
+    available = BooleanField("Available")
+    
